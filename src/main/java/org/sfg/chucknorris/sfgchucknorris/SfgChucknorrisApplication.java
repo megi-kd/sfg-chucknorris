52727a92517ecb6abc70c4658a2accda04fac7ee
Magdalena <mkceva@gmail.com>
2022-11-06 00:04:22 +0100

    Initial commit

---
+++ src/main/java/org/sfg/chucknorris/sfgchucknorris/SfgChucknorrisApplication.java
@@ -0,0 +1,13 @@
+package org.sfg.chucknorris.sfgchucknorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgChucknorrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgChucknorrisApplication.class, args);
+	}
+
+}
